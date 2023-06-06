d646ef266e999d84eeaf1fc50975afa97100490e
Charlotte Currey <charlottecurrey7@gmail.com>
2023-06-06 12:28:16 -0400

    changed header

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout } from '../../app/store';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store";
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -9,13 +9,13 @@ const Navbar = () => {
   const navigate = useNavigate();
   const logoutAndRedirectHome = () => {
     dispatch(logout());
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <div>
-      <h1>FS-App-Template</h1>
       <nav>
+        <h1>JCC Flower Shop</h1>
         {isLoggedIn ? (
           <div>
             {/* The navbar will show these links after you log in */}
