d3c0dc6f85c5d97fc0bd9179cb2bba44f1689163
Jessica Rogado <j.rogado17@yahoo.com>
2023-06-08 12:18:53 -0400

    added styling and updated the price for each product in the cart to multiply the base price with the quanitity to show the total price

--- .history/client/features/cart/Cart_20230608121343.js
+++ .history/client/features/cart/Cart_20230608121343.js
@@ -30,52 +30,43 @@ const Cart = () => {
 
   return (
     <div>
-      <h2 id="header-cart">Cart</h2>
+      <h2>Cart</h2>
       {cartItems.length === 0 ? (
         <p>No items in the cart</p>
       ) : (
-        <div className="cart">
+        <div class='cart'>
           <ul id="cart-items">
             <li id="cart-header">
               <div className="cart-item-header">Flower</div>
               <div className="cart-item-header">Quantity</div>
               <div className="cart-item-header">Price</div>
             </li>
-            {cartItems.map((item) => {
-              const quantity = quantities[item.id] || 1; // Default to 1 if no quantity selected
-              const totalPrice = item.price * quantity;
-
-              return (
-                <li key={item.id} className="cart-item">
-                  <div className="cart-item-name">{item.name}</div>
-                  <div>
-                    <select
-                      value={quantities[item.id] || 1}
-                      onChange={(e) =>
-                        handleQuantityChange(item.id, parseInt(e.target.value))
-                      }
-                    >
-                      {Array.from({ length: 10 }, (_, i) => (
-                        <option key={i + 1} value={i + 1}>
-                          {i + 1}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div className="cart-item-price">
-                    ${totalPrice.toFixed(2)}
-                  </div>
-                </li>
-              );
-            })}
+            {cartItems.map((item) => (
+              <li key={item.id} className="cart-item">
+                <div className="cart-item-name">{item.name}</div>
+                <div>
+                  <select
+                    value={quantities[item.id] || 1}
+                    onChange={(e) =>
+                      handleQuantityChange(item.id, parseInt(e.target.value))
+                    }
+                  >
+                    {Array.from({ length: 10 }, (_, i) => (
+                      <option key={i + 1} value={i + 1}>
+                        {i + 1}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="cart-item-price">${item.price.toFixed(2)}</div>
+              </li>
+            ))}
           </ul>
-          <div id="cost-box">
+          <div id='cost-box'>
             <p>Subtotal: ${calculateTotal()}</p>
             <p>Tax (10%): ${(calculateTotal() * 0.1).toFixed(2)}</p>
             <p>Shipping: $5.95</p>
-            <p id="total-amount">
-              Total: ${(parseFloat(calculateTotal()) + 5.96).toFixed(2)}
-            </p>
+            <p id='total-amount'>Total: ${(parseFloat(calculateTotal()) + 5.96).toFixed(2)}</p>
           </div>
         </div>
       )}
